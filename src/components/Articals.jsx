b7684263512b458def43a582d964480a4f620475
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-07-22 15:00:50 +0530

    infinite scroll added

--- src/components/Articals.jsx
+++ src/components/Articals.jsx
@@ -1,27 +1,55 @@
-import { Box } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-
-//components
+import { Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import Artical from './Artical';
 import { data } from '../../data';
 
 const Articals = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    const [news,setNews]=useState([]);
+  useEffect(() => {
+    // Fetch initial data when the component mounts
+    fetchNews();
+  }, []);
 
-    useEffect(()=>{
-        setNews(data)
-    },[]);
+  const fetchNews = () => {
+    // Simulate API call with a delay for demonstration purposes
+    setLoading(true);
+    setTimeout(() => {
+      // Assuming data is an array of new articles
+      setNews((prevNews) => [...prevNews, ...data]);
+      setLoading(false);
+    }, 0.8000);
+  };
+
+  const handleScroll = () => {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    const threshold = 100; // Adjust this value based on your needs
+    const atBottom = scrollTop + clientHeight >= scrollHeight - threshold;
+
+    if (atBottom && !loading) {
+      fetchNews();
+    }
+  };
+
+  useEffect(() => {
+    // Add the scroll event listener when the component mounts
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [loading]);
 
   return (
     <Box>
-        {
-          news.map((data)=>(
-            <Artical key={data.id} data={data}/>
-          ))
-        }
+      {news.map((data) => (
+        <Artical key={data.id} data={data} />
+      ))}
+      {loading && <p>Loading more articles...</p>}
     </Box>
-  )
-}
+  );
+};
 
 export default Articals;
