808fe42e05b9e222624484a43318afdf8e1ccf9d
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-07-22 08:05:53 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import Header from './components/Header'
-import InfoHeader from './components/InfoHeader'
-import {Box} from '@mui/material'
+import React from "react";
+import Header from "./components/Header";
+import InfoHeader from "./components/InfoHeader";
+import { Box,styled } from "@mui/material";
+
+//style
+const Container=styled(Box)`
+    width:60%;
+    margin:30px auto 0 auto;
+
+`
 
 const App = () => {
   return (
     <Box>
-      <Header/>
-      <InfoHeader/>
+      <Header />
+      <Container>
+        <InfoHeader />
+      </Container>
     </Box>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/InfoHeader.jsx
+++ src/components/InfoHeader.jsx
@@ -5,17 +5,39 @@ import {Box,Typography,styled} from '@mui/material'
 const appleStore = 'https://assets.inshorts.com/website_assets/images/appstore.png';
 const googleStore = 'https://assets.inshorts.com/website_assets/images/playstore.png';
 
+//stylled
+const Container=styled(Box)`
+    background:#f44336;
+    color:#ffffff;
+    display:flex;
+    align-items:center;
+    height:48px
+`
+const Image = styled('img')({
+    height:40,
+    '&:last-child':{
+        margin:'0 50px 0 20px'
+    }
+
+});
+
+const Text =styled(Typography)`
+    font-size:14px;
+    font-weight:300;
+    margin-left:50px
+`
+
 const InfoHeader = () => {
   return (
-    <Box>
-        <Typography>
+    <Container>
+        <Text>
             For the best experience use inshorts app on your smartphone
-        </Typography>
-        <Box>
-            <img src={appleStore} alt="applestore" />
-            <img src={googleStore} alt="googlestore" />
+        </Text>
+        <Box style={{display:'flex', marginLeft:'auto'}}>
+            <Image src={appleStore} alt="applestore" />
+            <Image src={googleStore} alt="googlestore" />
         </Box>
-    </Box>
+    </Container>
   )
 }
 
